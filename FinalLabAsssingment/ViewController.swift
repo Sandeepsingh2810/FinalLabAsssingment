61c55e99c8f3894175670a48c3bcb54d1eecffcd
Karan <kumarkay.ca@gmail.com>
2020-01-24 17:55:21 -0500

    Initial Commit

---
+++ FinalLabAsssingment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinalLabAsssingment
+//
+//  Created by Sandeep Jangra on 2020-01-24.
+//  Copyright © 2020 Sandeep. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
